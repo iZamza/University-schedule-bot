132fcbb75a60566601a4183162862f2f7890ce69
Viktar_Tsikhmanovich <Viktar_Tsikhmanovich@epam.com>
2020-08-03 23:32:49 +0300

    Add json, add first files

--- src/index.py
+++ src/index.py
@@ -1,26 +1,53 @@
 import telepot
 import time
+import os
 from telepot.loop import MessageLoop
+import json
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
-bot = telepot.Bot('1397222187:AAFoDRbVrr8TdeeuR_UnFIPzC2U9nbF9rFo')
+# get telegram bot token
+current_dir = os.curdir
+with open(os.path.join('..', '..', 'token.txt')) as f:
+    bot = telepot.Bot(f.readline())
+
+# to get info about bot delete '#' from next line
+# print(bot.getMe())
 
 
-# print(bot.getMe()) get info about bot
+def send_answer(chat_id, text, reply_markup=''):
+        bot.sendMessage(chat_id, text, reply_markup=reply_markup)
 
+def run_application(content_type, msg, chat_id):
+    if content_type == 'text':
+        if msg['text'] == '/start':
+            keyboard_markup = ReplyKeyboardMarkup(
+                keyboard=[
+                    [KeyboardButton(text="Получить расписание")],
+                    [KeyboardButton(text="Узнать тему занятия на завтра")],
+                    [KeyboardButton(text="Узнать тему занятия на сегодня")],
+                    [KeyboardButton(text="Оставить отзыв")]
+                ]
+            )
+            send_answer(chat_id, 'Добро пожаловать! Пожалуйста, выбирите ', keyboard_markup)
+        elif msg['text'] == 'Получить расписание':
+            send_answer(chat_id, 'Следующее занятие 01.09. Тема: введение в сосудистую хирургию')
+        elif msg['text'] == 'Узнать тему занятия на завтра':
+            send_answer(chat_id, 'Тема: введение в сосудистую хирургию')
+        elif msg['text'] == 'Узнать тему занятия на сегодня':
+            send_answer(chat_id, 'Тема: Варикозное расширение вен нижних конечностей')
+        else:
+            send_answer(chat_id, f'You type {msg["text"]}')
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
+    print(msg)
 
-    def send_answer(text):
-        bot.sendMessage(chat_id, text)
+    with open('users.json') as users_storage:
+        users_data = json.load(users_storage)
 
-    if content_type == 'text':
-        if msg['text'] == '/start':
-            send_answer('Добро пожаловать!')
-        elif msg['text'].lower() == 'hi':
-            send_answer('Hihihi!')
-        else:
-            send_answer(f'You type {msg["text"]}')
+    for user in users_data:
+        if user['user_id'] == msg['from']['id']:
+            run_application(content_type, msg, chat_id)
 
 
 MessageLoop(bot, handle).run_as_thread()
