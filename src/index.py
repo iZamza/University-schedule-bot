ddd055c5d8489d11d8d3677b9252d330ae094d39
Viktar_Tsikhmanovich <Viktar_Tsikhmanovich@epam.com>
2020-08-04 22:39:05 +0300

    add roles

--- src/index.py
+++ src/index.py
@@ -18,22 +18,25 @@ with open(os.path.join('..', '..', 'token.txt')) as f:
 def register_user(content_type, msg, chat_id):
     if content_type == 'text':
         if msg['text'] != '/start':
-            print(msg)
             current_group = ''.join(filter(str.isdigit, msg['text']))
             if len(current_group) == 1:
                 current_group = f"60{current_group}"
             elif len(current_group) == 2:
                 current_group = f"6{current_group}"
-            with open("users.json", "r+") as users_storage:
-                users_data = json.load(users_storage)
-                users_data.append({
-                    "user_id": msg['from']['id'],
-                    "user_group": current_group,
-                    "user_name": msg['from']['first_name'],
-                    "user_feedback": []})
-                users_storage.seek(0)
-                json.dump(users_data, users_storage)
-            run_application(content_type, msg, chat_id)
+
+            if len(current_group) == 3:
+                with open("users.json", "r+") as users_storage:
+                    users_data = json.load(users_storage)
+                    users_data.append({
+                        "id": msg['from']['id'],
+                        "group": current_group,
+                        "name": msg['from']['first_name'],
+                        "role": 'student',
+                        "feedback": []
+                    })
+                    users_storage.seek(0)
+                    json.dump(users_data, users_storage)
+                    run_student_dialogue(content_type, msg, chat_id, users_data[-1])
         else:
             send_answer(chat_id, 'Пожалуйста, введите номер группы ')
 
@@ -42,17 +45,35 @@ def send_answer(chat_id, text, reply_markup=''):
     bot.sendMessage(chat_id, text, reply_markup=reply_markup)
 
 
-def run_application(content_type, msg, chat_id):
+def run_student_dialogue(content_type, msg, chat_id, current_user):
+    print(current_user)
     if content_type == 'text':
-        keyboard_markup = ReplyKeyboardMarkup(
-            keyboard=[
-                [KeyboardButton(text="Получить расписание")],
-                [KeyboardButton(text="Узнать тему занятия на завтра")],
-                [KeyboardButton(text="Узнать тему занятия на сегодня")],
-                [KeyboardButton(text="Оставить отзыв")]
-            ]
-        )
-        send_answer(chat_id, 'Добро пожаловать! Пожалуйста, выбирите ', keyboard_markup)
+        if msg['text'] == 'bla':
+            print('bla')
+        else:
+            keyboard_markup = ReplyKeyboardMarkup(
+                keyboard=[
+                    [KeyboardButton(text="Получить расписание")],
+                    [KeyboardButton(text="Узнать тему занятия на завтра")],
+                    [KeyboardButton(text="Узнать тему занятия на сегодня")],
+                    [KeyboardButton(text="Оставить отзыв")]
+                ]
+            )
+            send_answer(chat_id, 'Добро пожаловать! Пожалуйста, выбирите ', keyboard_markup)
+
+
+def run_admin_dialogue(content_type, msg, chat_id, current_user):
+    print(current_user)
+    if content_type == 'text':
+        if msg['text'] == 'bla':
+            print('bla')
+        else:
+            keyboard_markup = ReplyKeyboardMarkup(
+                keyboard=[
+                    [KeyboardButton(text="Отправить пушку!")],
+                ]
+            )
+            send_answer(chat_id, 'Жги, админ!', keyboard_markup)
 
 
 def handle(msg):
@@ -64,11 +85,14 @@ def handle(msg):
     current_user = []
 
     for user in users_data:
-        if user['user_id'] == msg['from']['id']:
+        if user['id'] == msg['from']['id']:
             current_user.append(user)
 
     if len(current_user):
-        run_application(content_type, msg, chat_id)
+        if current_user[0]['role'] == 'admin':
+            run_admin_dialogue(content_type, msg, chat_id, current_user[0])
+        else:
+            run_student_dialogue(content_type, msg, chat_id, current_user[0])
     else:
         register_user(content_type, msg, chat_id)
 
