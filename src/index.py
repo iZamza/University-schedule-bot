fa562b304eebce16d8342051f7191879d96e990b
Viktar_Tsikhmanovich <Viktar_Tsikhmanovich@epam.com>
2020-08-04 17:59:28 +0300

    add possibility to add new user

--- src/index.py
+++ src/index.py
@@ -18,8 +18,22 @@ with open(os.path.join('..', '..', 'token.txt')) as f:
 def register_user(content_type, msg, chat_id):
     if content_type == 'text':
         if msg['text'] != '/start':
+            print(msg)
             current_group = ''.join(filter(str.isdigit, msg['text']))
-            print(current_group)
+            if len(current_group) == 1:
+                current_group = f"60{current_group}"
+            elif len(current_group) == 2:
+                current_group = f"6{current_group}"
+            with open("users.json", "r+") as users_storage:
+                users_data = json.load(users_storage)
+                users_data.append({
+                    "user_id": msg['from']['id'],
+                    "user_group": current_group,
+                    "user_name": msg['from']['first_name'],
+                    "user_feedback": []})
+                users_storage.seek(0)
+                json.dump(users_data, users_storage)
+            run_application(content_type, msg, chat_id)
         else:
             send_answer(chat_id, 'Пожалуйста, введите номер группы ')
 
@@ -30,21 +44,19 @@ def send_answer(chat_id, text, reply_markup=''):
 
 def run_application(content_type, msg, chat_id):
     if content_type == 'text':
-        if msg['text'] == '/start':
-            keyboard_markup = ReplyKeyboardMarkup(
-                keyboard=[
-                    [KeyboardButton(text="Получить расписание")],
-                    [KeyboardButton(text="Узнать тему занятия на завтра")],
-                    [KeyboardButton(text="Узнать тему занятия на сегодня")],
-                    [KeyboardButton(text="Оставить отзыв")]
-                ]
-            )
-            send_answer(chat_id, 'Добро пожаловать! Пожалуйста, выбирите ', keyboard_markup)
+        keyboard_markup = ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Получить расписание")],
+                [KeyboardButton(text="Узнать тему занятия на завтра")],
+                [KeyboardButton(text="Узнать тему занятия на сегодня")],
+                [KeyboardButton(text="Оставить отзыв")]
+            ]
+        )
+        send_answer(chat_id, 'Добро пожаловать! Пожалуйста, выбирите ', keyboard_markup)
 
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    print(msg)
 
     with open('users.json') as users_storage:
         users_data = json.load(users_storage)
