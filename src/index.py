b99943decc06e78b7e3722a3026fe0fd720d30a6
Viktar_Tsikhmanovich <Viktar_Tsikhmanovich@epam.com>
2020-08-05 14:16:58 +0300

    Add simple schedule

--- src/index.py
+++ src/index.py
@@ -16,6 +16,10 @@ with open(os.path.join('..', '..', 'token.txt')) as f:
 
 
 def register_user(content_type, msg, chat_id):
+    user_name = msg['from']['first_name']
+    with open('groups.json') as groups:
+        groups_data = json.load(groups)
+
     if content_type == 'text':
         if msg['text'] != '/start':
             current_group = ''.join(filter(str.isdigit, msg['text']))
@@ -24,19 +28,21 @@ def register_user(content_type, msg, chat_id):
             elif len(current_group) == 2:
                 current_group = f"6{current_group}"
 
-            if len(current_group) == 3:
+            if len(current_group) == 3 and current_group in groups_data.keys():
                 with open("users.json", "r+") as users_storage:
                     users_data = json.load(users_storage)
                     users_data.append({
                         "id": msg['from']['id'],
                         "group": current_group,
-                        "name": msg['from']['first_name'],
-                        "role": 'student',
-                        "feedback": []
+                        "name": user_name,
+                        "role": 'student'
                     })
                     users_storage.seek(0)
                     json.dump(users_data, users_storage)
                     run_student_dialogue(content_type, msg, chat_id, users_data[-1])
+            else:
+                send_answer(chat_id, f'{user_name}, Вы уверены, что ввели правильный номер группы? '
+                                     f'Пожалуйста, введите номер группы')
         else:
             send_answer(chat_id, 'Пожалуйста, введите номер группы ')
 
@@ -46,20 +52,32 @@ def send_answer(chat_id, text, reply_markup=''):
 
 
 def run_student_dialogue(content_type, msg, chat_id, current_user):
-    print(current_user)
     if content_type == 'text':
-        if msg['text'] == 'bla':
-            print('bla')
+        with open('lessons.json', encoding='utf8') as lessons:
+            lessons_data = json.load(lessons)
+
+        if msg['text'] == 'Получить расписание занятий на весь цикл':
+            send_answer(chat_id, '1. занятие тогда-то')
+
+        if msg['text'] in lessons_data.keys():
+            send_answer(chat_id, lessons_data[msg['text']])
+
+        if msg['text'] == 'Получить вопросы к занятию':
+            lessons_list = []
+            for lesson in lessons_data:
+                lessons_list.append([KeyboardButton(text=f'{lesson}')])
+            keyboard_markup = ReplyKeyboardMarkup(
+                keyboard=lessons_list
+            )
+            send_answer(chat_id, f'{current_user["name"]}! Выберите тему занятия', keyboard_markup)
         else:
             keyboard_markup = ReplyKeyboardMarkup(
                 keyboard=[
-                    [KeyboardButton(text="Получить расписание")],
-                    [KeyboardButton(text="Узнать тему занятия на завтра")],
-                    [KeyboardButton(text="Узнать тему занятия на сегодня")],
-                    [KeyboardButton(text="Оставить отзыв")]
+                    [KeyboardButton(text="Получить расписание занятий на весь цикл")],
+                    [KeyboardButton(text="Получить вопросы к занятию")]
                 ]
             )
-            send_answer(chat_id, 'Добро пожаловать! Пожалуйста, выбирите ', keyboard_markup)
+            send_answer(chat_id, f'{current_user["name"]}! Выберите, что бы Вы хотели узнать', keyboard_markup)
 
 
 def run_admin_dialogue(content_type, msg, chat_id, current_user):
